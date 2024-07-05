fb7a862c10b5fd36269e8ec99a0ff7928b445ec6
hadeel-farhan <hadeelf2001@gmail.com>
2024-07-05 21:38:20 +0400

    client testimonial

--- js/slideshow.js
+++ js/slideshow.js
@@ -1,14 +1,22 @@
-var slideIndex = 0;
-carousel();
+var slideIndex1 = 0;
+var slideIndex2 = 0;
+carousel("mySlides", slideIndex1, 1);
+carousel("mySlides2", slideIndex2, 2);
 
-function carousel() {
+function carousel(className, slideIndex, carouselNumber) {
     var i;
-    var x = document.getElementsByClassName("mySlides");
+    var x = document.getElementsByClassName(className);
     for (i = 0; i < x.length; i++) {
         x[i].style.display = "none";
     }
     slideIndex++;
     if (slideIndex > x.length) { slideIndex = 1 }
     x[slideIndex - 1].style.display = "block";
-    setTimeout(carousel, 2000); // Change image every 2 seconds
+    setTimeout(function () {
+        if (carouselNumber === 1) {
+            carousel("mySlides", slideIndex, 1);
+        } else {
+            carousel("mySlides2", slideIndex, 2);
+        }
+    }, 2000); // Change image every 2 seconds
 }
